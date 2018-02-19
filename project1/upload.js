589fbd7c2dc33d4c3ec606108cd2ac704c9e3850
Danny <36552981+danielmaryanski@users.noreply.github.com>
2018-02-18 19:49:21 -0500

    added some wonky javascript

---
+++ project1/upload.js
@@ -0,0 +1,10 @@
+add_tag_btn = document.getElementById('add-tag');
+
+function addTag(){
+	var tags = document.getElementById("upload-tags");
+	var new_tag = document.createElement('div');	
+	new_tag.setAttribute('class', 'form-group row upload-tag');
+	new_tag.innerHTML = document.getElementById("new-tag").innerHTML;
+	tags.appendChild(new_tag);
+}
+
