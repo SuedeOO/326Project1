1cff242abdd6487ef66b8c5bf57484df750789c2
Matt Robinson <mattrobinson125@gmail.com>
2018-03-06 15:18:31 -0500

    add mix model

--- thekrustykrab/mixlist/models.py
+++ thekrustykrab/mixlist/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Mix(models.Model):
+    '''
+    Represents a Mix, an audio file uploaded with associated metadata tags
+    '''
+    audio_file = models.FileField()
+    title = models.CharField(max_length=50)
+    # uploader = models.ForeignKey('User', on_delete=models.CASCADE)
+    length = models.DurationField()
+    upload_date = models.DateTimeField()
+    play_count = models.IntegerField()
+    # playlist = ...
+
+    def __str__(self):
+        return self.title
