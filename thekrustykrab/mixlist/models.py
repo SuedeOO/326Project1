82e1a3fe42e6e1d09fe1479ff23d4afb727b7925
Danny <36552981+danielmaryanski@users.noreply.github.com>
2018-03-06 21:03:43 -0500

    Added track and external link models
    They probably still need some work

--- thekrustykrab/mixlist/models.py
+++ thekrustykrab/mixlist/models.py
@@ -15,3 +15,28 @@ class Mix(models.Model):
 
     def __str__(self):
         return self.title
+        
+        
+class Track(models.Model):
+    """
+    A track, may be referenced by multiple Mixes
+    """
+    title = models.CharField(max_length = 100)
+    artist = models.CharField(max_length = 50)
+    extra_info = models.CharField(max_length = 100) #remixer, etc.
+    links = models.ManyToManyField('ExternalLink')
+    reference_count = models.IntegerField() #how mixes reference this track
+	
+	def __str__(self):
+		return str(self.artist) + " - " + str(self.title)
+    
+class ExternalLink(models.Model):
+    """
+    A link to where a track can be found elsewhere on the internet.
+    """
+	provider = models.CharField(max_length = 50)
+	url = models.URLField(max_length = 200)
+	
+	def __str__(self):
+		return self.url
+    
