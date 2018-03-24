8fac7f181e1cb713826aca63b7e7dc70e5819a2e
Matt Robinson <mattrobinson125@gmail.com>
2018-03-23 20:22:44 -0400

    encode the Mix-Track relationship with an intermediate model... have to wipe the db :(

--- thekrustykrab/mixlist/admin.py
+++ thekrustykrab/mixlist/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
-from .models import Mix, Track, ExternalLink, Profile
+from .models import Mix, Track, ExternalLink, PlaylistMembership, Profile
 
 # Register your models here.
-admin.site.register(Mix)
 admin.site.register(Track)
 admin.site.register(ExternalLink)
+
+class PlaylistMembershipInline(admin.TabularInline):
+    model = PlaylistMembership
+    extra = 1
+
+@admin.register(Mix)
+class MixAdmin(admin.ModelAdmin):
+	inlines = (PlaylistMembershipInline,)
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
--- thekrustykrab/mixlist/models.py
+++ thekrustykrab/mixlist/models.py
@@ -1,47 +1,4 @@
 from django.db import models
-
-# Create your models here.
-class Mix(models.Model):
-    '''
-    Represents a Mix, an audio file uploaded with associated metadata tags
-    '''
-    audio_file = models.FileField() # TODO: Pass in a storage param
-    title = models.CharField(max_length=80)
-    slug = models.SlugField() # title_of_song_with_underscores_as_spaces
-    # uploader = models.ForeignKey('User', on_delete=models.CASCADE)
-    length = models.DurationField()
-    upload_date = models.DateTimeField(auto_now_add=True)
-    play_count = models.IntegerField()
-    playlist = models.ManyToManyField('Track')
-
-    def __str__(self):
-        return self.title
-        
-        
-class Track(models.Model):
-    """
-    A track, may be referenced by multiple Mixes
-    """
-    title = models.CharField(max_length = 100)
-    artist = models.CharField(max_length = 50)
-    extra_info = models.CharField(max_length = 100, blank=True, null=True) #remixer, etc.
-    links = models.ManyToManyField('ExternalLink')
-    reference_count = models.IntegerField() #how mixes reference this track
-    
-    def __str__(self):
-        return str(self.artist) + " - " + str(self.title)
-    
-class ExternalLink(models.Model):
-    """
-    A link to where a track can be found elsewhere on the internet.
-    """
-    provider = models.CharField(max_length = 50)
-    url = models.URLField(max_length = 200)
-    
-    def __str__(self):
-        return self.url
-
-
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -73,3 +30,63 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+
+class Mix(models.Model):
+    '''
+    Represents a Mix, an audio file uploaded with associated metadata tags
+    '''
+    audio_file = models.FileField() # TODO: Pass in a storage param
+    title = models.CharField(max_length=80)
+    slug = models.SlugField() # title_of_song_with_underscores_as_spaces
+    uploader = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="mixes")
+    length = models.DurationField()
+    upload_date = models.DateTimeField(auto_now_add=True)
+    play_count = models.IntegerField()
+    playlist = models.ManyToManyField('Track', through='PlaylistMembership')
+
+    def __str__(self):
+        return self.title
+
+        
+class Track(models.Model):
+    """
+    A track, may be referenced by multiple Mixes
+    """
+    title = models.CharField(max_length = 100)
+    artist = models.CharField(max_length = 50)
+    extra_info = models.CharField(max_length = 100, blank=True, null=True) #remixer, etc.
+    reference_count = models.IntegerField() #how mixes reference this track
+    
+    def __str__(self):
+        return str(self.artist) + " - " + str(self.title)
+
+
+class ExternalLink(models.Model):
+    """
+    A link to where a track can be found elsewhere on the internet.
+    """
+    track = models.ForeignKey(Track, on_delete=models.CASCADE, related_name='links')
+    provider = models.CharField(max_length = 50)
+    url = models.URLField(max_length = 200)
+    
+    def __str__(self):
+        return self.url
+
+
+class PlaylistMembership(models.Model):
+    '''
+    Models the (many to many) relationship between a Mix and its tracks
+    '''
+    mix = models.ForeignKey(Mix, on_delete=models.CASCADE)
+    track = models.ForeignKey(Track, on_delete=models.CASCADE)
+    # the time in the mix that the track occurs
+    time = models.TimeField() 
+
+    def __str__(self):
+        return str(self.mix.title) + '--' + str(self.track.title) + '@' + str(self.time)
+    
+
+
+
+
