a5fb138b20c21b785c8111f79e6ab993e1bd12d0
DSSzymanski <dszymanski@umass.edu>
2018-03-26 19:03:08 -0400

    added view

--- thekrustykrab/mixlist/views.py
+++ thekrustykrab/mixlist/views.py
@@ -7,10 +7,13 @@ class MixView(generic.DetailView):
     model = Mix
     template_name = 'mix_detail.html'
 
-
 class EditMixView(generic.DetailView):
     model = Mix
     template_name = 'editor_edit.html'
     
 class UploadMixView(generic.TemplateView):
     template_name = 'editor_upload.html'
+
+class ProfileView(generic.DetailView):
+	model = Profile
+	template_name = 'profile_template.html'
