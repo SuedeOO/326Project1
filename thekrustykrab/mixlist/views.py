675a019861cc2b6244c5ccfb3da42c486320eae8
DSSzymanski <dszymanski@umass.edu>
2018-03-26 20:51:25 -0400

    added forgoten import

--- thekrustykrab/mixlist/views.py
+++ thekrustykrab/mixlist/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Mix
+from .models import Mix, Profile
 
 # Create your views here.
 class MixView(generic.DetailView):
