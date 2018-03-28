4ac13ba1f6629544f37ab7ac4c81e5ddb2b0a541
Matt Robinson <mattrobinson125@gmail.com>
2018-03-27 23:18:22 -0400

    added new data

--- thekrustykrab/mixlist/models.py
+++ thekrustykrab/mixlist/models.py
@@ -38,7 +38,8 @@ class Mix(models.Model):
     '''
     audio_file = models.FileField() # TODO: Pass in a storage param
     title = models.CharField(max_length=80)
-    artist = models.CharField(max_length=80, null=True)
+    artist = models.CharField(max_length=80, blank=True, null=True)
+    image = models.FileField(blank=True, null=True)
     slug = models.SlugField() # title_of_song_with_underscores_as_spaces
     uploader = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="mixes")
     length = models.DurationField()
