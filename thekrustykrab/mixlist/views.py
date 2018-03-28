b00350a0251a1df16de0771bb1b97d3914072079
Matt Robinson <mattrobinson125@gmail.com>
2018-03-27 23:27:50 -0400

    context data for mix view

--- thekrustykrab/mixlist/views.py
+++ thekrustykrab/mixlist/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Mix, Profile
+from django.forms.models import model_to_dict
 
 # Create your views here.
 class MixView(generic.DetailView):
@@ -9,7 +10,17 @@ class MixView(generic.DetailView):
     
     def get_context_data(self, **kwargs):
         context = super(MixView, self).get_context_data(**kwargs)
-        context['playlist'] = ['this', 'is', 'array']
+        playlist = []
+        for member in context['mix'].playlistmembership_set.select_related().prefetch_related().all():
+            playlist.append({
+                'id': member.track.id,
+                'title': member.track.title,
+                'artist': member.track.artist,
+                'time': member.time.total_seconds(),
+                'time_str': str(member.time),
+                'links': [model_to_dict(link, fields=('provider', 'url')) for link in member.track.links.all()]
+            })
+        context['playlist'] = playlist
         return context
 
 
