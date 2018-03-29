3fa2294ba76bb5c986f33be87bfd4b0f08c6508e
Matt Robinson <mattrobinson125@gmail.com>
2018-03-28 20:03:32 -0400

    fixed mix links, added some bootstrap styling

--- thekrustykrab/mixlist/models.py
+++ thekrustykrab/mixlist/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.urls import reverse
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -52,6 +53,8 @@ class Mix(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('mix-detail', args=[str(self.slug)])
         
 class Track(models.Model):
     """
