7523c28586c590c3141cc8045df934ea3ff48879
Danny <36552981+danielmaryanski@users.noreply.github.com>
2018-03-29 16:42:00 -0400

    Improved scripting
    Still iffy, I think there's some bug with jQuery

--- thekrustykrab/mixlist/static/mixlist/upload.js
+++ thekrustykrab/mixlist/static/mixlist/upload.js
@@ -2,12 +2,21 @@ $("#tag-add").click(function(){
 	addEmptyTag();
 });
 
+tags = [];
+
+function uniqueID() {
+  return Math.round(new Date().getTime() + (Math.random() * 100));
+}
+
 function addEmptyTag(){
 	addTag("","","","");
 }
 
 function addTag(startTime, title, artist, links){
 	newhtml = $("#tag-new").html();
+	newid = uniqueID();
+	newhtml = newhtml.replace("¶id", newid);
+	tags.push(newid);
 	newhtml = newhtml.replace("¶startTime", startTime);
 	newhtml = newhtml.replace("¶title", title);
 	newhtml = newhtml.replace("¶artist", artist);
@@ -17,12 +26,23 @@ function addTag(startTime, title, artist, links){
 }
 
 function setFunctions(){
-	$(".tag-up").click(function() {
-		$(this).parent().parent().prev().insertAfter($(this).parent().parent());			
+	/*
+	for(id in tags){		
+		id_select = "#" + id;
+		//$(id_select + " > div > .tag-up").attr("pid", id_select);
+		$(id_select + " > div > .tag-up").click(function() {			
+			$(id_select).remove();//.prev().insertAfter($(id_select));			
+		});
+	}
+	*/
+	$(".tag-up").click(function(e) {
+		tag = $(this).parent().parent();
+		tag.prev(".tag").insertAfter(tag);
 	});
 	
-	$(".tag-down").click(function() {
-		$(this).parent().parent().next().insertBefore($(this).parent().parent());		
+	$(".tag-down").click(function(e) {
+		tag = $(this).parent().parent();
+		tag.next(".tag").insertBefore(tag);	
 	});
 	
 	$(".tag-remove").click(function() {
