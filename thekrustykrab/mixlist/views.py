284448c08772bba00c4c6142567d193c0ea44082
GK67 <32550836+GK67@users.noreply.github.com>
2018-04-16 20:50:52 -0400

    Add files via upload

--- thekrustykrab/mixlist/views.py
+++ thekrustykrab/mixlist/views.py
@@ -5,15 +5,6 @@ from django.forms.models import model_to_dict
 from django.http import HttpRequest
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from django.urls import reverse
-from django.utils.text import slugify
-from datetime import timedelta
-from django.contrib.auth.forms import UserChangeForm 
-from django.contrib.auth.models import User
-from mixlist.forms import (
-    EditProfileForm,
-    UserForm
-)
 
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
@@ -41,6 +32,7 @@ class MixView(generic.DetailView):
         context['playlist'] = playlist
         return context
 
+
 class LogInView(generic.TemplateView):
     template_name = 'login_signup.html'
 
@@ -50,20 +42,6 @@ class EditMixView(generic.DetailView):
     
 class UploadMixView(generic.TemplateView):
     template_name = 'editor_upload.html'
-
-# New upload views here
-#from .forms import UploadMixModelForm
-class CreateMixView(generic.CreateView):
-    model = Mix
-    template_name = 'mix_upload.html'
-    fields = ('title', 'audio_file')
-
-    def form_valid(self, form):
-        form.instance.slug = slugify(form.instance.title)
-        form.instance.uploader = self.request.user.profile
-        form.instance.length = timedelta()
-        return super(CreateMixView, self).form_valid(form)
-
 
 class ProfileView(generic.DetailView):
     model = Profile
@@ -95,20 +73,9 @@ class ChartsView(generic.ListView):
     template_name = 'charts_template.html'
     queryset = Mix.objects.order_by('-play_count')
 
-
-def edit_profile(request):
-    if request.method == 'POST':
-        profile_form = EditProfileForm(request.POST, instance = request.user.profile)
-        user_form = UserForm(request.POST, instance=request.user)
-        if profile_form.is_valid() and user_form.is_valid():
-            profile_form.save()
-            user_form.save() 
-            return redirect('/profile/1')
-    else:
-        profile_form = EditProfileForm(instance = request.user.profile)
-        user_form=UserForm(instance = request.user)
-        return render(request,'edit_profile.html', {'user_form': user_form,'profile_form': profile_form})
-
+class EditProfileView(generic.TemplateView):
+    model = Profile
+    template_name = 'edit_profile.html'
 
 def follow(request, profile_id):
     user = request.user.profile
@@ -122,3 +89,18 @@ def unfollow(request, profile_id):
     profile = Profile.objects.get(id=profile_id)
     Follows.objects.filter(owner=user, follows=profile).delete()
     return redirect("/profile/"+profile_id)
+
+def addFavorite(request, mix_id):
+    user = request.user.profile
+    user.favorites.add(mix_id)
+    user.save()
+    next = request.POST.get('next', '/')
+    return redirect(next)
+
+
+def removeFavorite(request, mix_id):
+    user = request.user.profile
+    user.favorites.delete(mix_id)
+    user.save()
+    next = request.POST.get('next', '/')
+    return redirect(next)
