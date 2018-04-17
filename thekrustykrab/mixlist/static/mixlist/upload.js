48ea0f2888466610146e3b24d014e6d4fc78f061
Matt Robinson <mattrobinson125@gmail.com>
2018-04-16 22:17:36 -0400

    Add an audio player to the edit mix page

--- thekrustykrab/mixlist/static/mixlist/upload.js
+++ thekrustykrab/mixlist/static/mixlist/upload.js
@@ -132,3 +132,37 @@ function compareTags(a, b){
         return 1;
 }
 
+
+let file = document.getElementById("audioFile");
+let togglePlay = (button) => {
+    if (file.paused) {
+        file.play();
+    } else {
+        file.pause();
+    }
+    button.classList.toggle("fa-play");
+    button.classList.toggle("fa-pause");
+}
+
+file.addEventListener("timeupdate", (timeUpdate) => {
+    let totalDuration = file.duration;
+    let timestamp = timeUpdate.target.currentTime;
+    updateTime(timestamp);
+    updateProgressBar(timestamp);
+});
+
+let updateTime = (timestamp) => {
+    var seconds = Math.floor(timestamp);
+    let minutes = Math.floor(seconds / 60);
+    seconds = seconds % 60;
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    document.getElementById("duration").innerHTML = minutes + ":" + seconds
+}
+
+let updateProgressBar = (timestamp) => {
+    let progressBar = document.getElementById("progressBar");
+    let pct = 100 * timestamp / file.duration;
+    progressBar.style.width = pct + "%";
+}
